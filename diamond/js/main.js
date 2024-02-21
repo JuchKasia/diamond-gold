2bb16a473ddc4f506b16dde5d71eac9655fde109
Katarzyna Juchniewicz <juchkasia@gmail.com>
2024-02-21 11:34:33 +0100

    feat: added blur and scroll, fixed function openMobileMenu

--- diamond/js/main.js
+++ diamond/js/main.js
@@ -1,19 +1,23 @@
-const navBtn = document.querySelector('.burger--btn');
+const blur = document.querySelector('.blur');
 const body = document.body;
+const navBtn = document.querySelector('.burger--btn');
 const navMobile = document.querySelector('.nav-mobile')
 
 /* BURGER MENU */
 const openMobileMenu = () => {
     navBtn.classList.toggle('active');
     navMobile.classList.toggle('active');
+    blur.classList.toggle('open');
+    body.classList.toggle('scroll');
 }
 
 navBtn.addEventListener('click', (openMobileMenu));
 
 
-function closeMenu() {
-    navBtn.classList.remove('active');
-}
+// function closeMenu() {
+//     navBtn.classList.remove('active');
+//     blur.classList.remove('open');
+// }
 
 window.addEventListener('resize', () => {
     if (document.documentElement.clientWidth > 767) {
